1424038e11183aa165ff14bc12b4ebb81a5d4752
JuanR <juan.roman.roche@gmail.com>
2025-08-29 17:15:14 +0200

    change popsize to none to use standard default

--- models/projection.py
+++ models/projection.py
@@ -18,7 +18,7 @@ class ProjectionModel:
         self,
         p: int = 2,
         sigma0: float = 0.5,
-        popsize: int | None = 21,
+        popsize: int | None = None,
         max_gen: int = 1000,
         seed: int | None = None,
         log_every: int = 0,
